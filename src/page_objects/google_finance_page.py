b9c921b6e8a89ab893aaa4fabf5b160354ac0816
Michael Andon <andon.m3@gmail.com>
2024-09-26 17:35:45 -0700

    feat: page factory pattern

--- src/page_objects/google_finance_page.py
+++ src/page_objects/google_finance_page.py
@@ -6,19 +6,21 @@ class GoogleFinancePage(BasePage):
     def __init__(self, driver, url="https://www.google.com/finance/"):
         super().__init__(driver, url)
 
-    def get_stock_symbols(self):
-        # find all ul elements matching
-        stock_elements = self.find_elements(
+    @property
+    def stock_elements(self):
+        """Simulate Page Factory behavior: find stock list items when accessed."""
+        return self.find_elements(
             By.CSS_SELECTOR,
-            "#yDmH0d > c-wiz.zQTmif.SSPGKf.ccEnac > div > div.e1AOyf > div > div > div.fAThCb > c-wiz:nth-child(1) > div > section > ul > li",
+            "#yDmH0d > c-wiz.zQTmif.SSPGKf.ccEnac > div > div.e1AOyf > div > div > div.fAThCb > c-wiz:nth-child(1) > div > section > ul > li"
         )
 
-        # extract and return only the stock symbol text
-        stock_symbols = []
-        for li in stock_elements:
+    @property
+    def stock_symbols(self):
+        """Simulate Page Factory behavior: extract stock symbols."""
+        symbols = []
+        for li in self.stock_elements:
             symbol_element = li.find_element(
                 By.CSS_SELECTOR, "a > div > div > div.iLEcy > div.rzR5Id > div > div"
             )
-            stock_symbols.append(symbol_element)
-
-        return stock_symbols
+            symbols.append(symbol_element.text)
+        return symbols
